a4f5ac3cc2bc464ce5367d4e27190ec5a6e92d03
sakilk130 <sakilk130@gmail.com>
2023-05-08 00:41:18 +0600

    feat: create button component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/ui/Button';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC = () => {
+  return (
+    <Button variant="default" size={'sm'}>
+      Hello
+    </Button>
+  );
+};
+
+export default page;
