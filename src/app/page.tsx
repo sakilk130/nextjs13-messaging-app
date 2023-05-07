9d1a6a308616f3320b5b8c2d1d56d9079e558c40
sakilk130 <sakilk130@gmail.com>
2023-05-08 01:54:44 +0600

    feat: login with google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { Inter } from 'next/font/google';
 
-const inter = Inter({
-  subsets: ['latin'],
-});
-
 export default async function Home() {
   return <div className="text-red-800">hello world</div>;
 }
